cd27f1d507df0db360825a6b291ca33711c9ad75
Hiitagiii <mirzaspc@gmail.com>
2021-01-08 18:55:48 +0700

    anykernel.sh: update and redesign script

--- anykernel.sh
+++ anykernel.sh
@@ -7,13 +7,13 @@ properties() { '
 kernel.string=
 do.devicecheck=1
 do.modules=0
-do.systemless=1
+do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=surya
 device.name2=karna
-device.name3=
-device.name4=
+device.name3=surya_in
+device.name4=karna_in
 device.name5=
 supported.versions=
 supported.patchlevels=
@@ -22,24 +22,31 @@ supported.patchlevels=
 # shell variables
 block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
-ramdisk_compression=auto;
-
+ramdisk_compression=none;
 
 ## AnyKernel methods (DO NOT CHANGE)
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-
-## AnyKernel file attributes
-# set permissions/ownership for included ramdisk files
-set_perm_recursive 0 0 755 644 $ramdisk/*;
-set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
-
 ## AnyKernel install
-#dump_boot;
+# dump_boot;
 split_boot;
 
-#write_boot;
+ui_print "";
+ui_print "==============================";
+ui_print "||       Support Group      ||";
+ui_print "||      t.me/azrim_lab      ||";
+ui_print "||    t.me/silont_support   ||";
+ui_print "==============================";
+ui_print "";
+
+if [ -f $split_img/ramdisk.cpio ]; then
+  unpack_ramdisk;
+  repack_ramdisk;
+fi;
+
 flash_boot;
 flash_dtbo;
+# backup_file init.rc;
+
 ## end install
